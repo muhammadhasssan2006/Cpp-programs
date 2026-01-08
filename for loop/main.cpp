bfc2eb0a7abd83df500b13ffde8468300ef2a33a
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-01-08 20:59:13 +0500

    for loop even table

--- for loop/main.cpp
+++ for loop/main.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
-    cout<<"enter any number for table";
-    cin>>num;
+    cout << "enter any number for table";
+    cin >> num;
     // table using foor loop
-    for(int i=1; i <= 10; i++){
-        cout<<num*i<<endl;
+    // for(int i=1; i <= 10; i++){
+    //     cout<<num*i<<endl;
+    // }
+
+    // table of even numbers
+
+    for (int i = 1; i <= 10; i++)
+    {
+
+        if (i % 2 == 0)
+        {
+            cout << num * i << endl;
+        }
     }
 }
