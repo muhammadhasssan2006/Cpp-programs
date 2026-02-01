407882c4a28af3ca925cdc224e33aa67f5728eb9
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-02-01 22:27:56 +0500

    array started

---
+++ Array/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // syntax of array
+    // int arr[5];
+    // 1)Write a program to store 5 integers in an array and print them.
+
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // 2)Take n elements from user and print their sum.
+    
+}
