dd82ecc0ae308f44000200a2648749b1a574ef74
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-02-24 20:28:15 +0500

    more practice done

--- final-program-practice/main.cpp
+++ final-program-practice/main.cpp
@@ -36,6 +36,34 @@ int main()
     //  }
     //  cout<<"result is: "<<result;
 
-    hasdi
+    // prime number
 
+    int num;
+    bool prime = true;
+
+    cout << "enter a number: ";
+    cin >> num;
+
+    if (num <= 1)
+    {
+        prime = false;
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+    if (prime)
+    {
+        cout << "prime number";
+    }
+
+    else{
+    cout<<"non prime number";
+
+    }
 }
