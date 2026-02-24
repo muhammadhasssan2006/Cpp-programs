514a8b51079a41a32002c88245e09703914b0d75
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-02-24 20:56:32 +0500

    more practice done

--- final-program-practice/main.cpp
+++ final-program-practice/main.cpp
@@ -69,5 +69,15 @@ int main()
 
     // fibonacci series 
 
-    
+  int num,a=0,b=1,next;
+  cout<<"enter a number: ";
+  cin>>num;
+
+  for(int i = 0; i <= num; i++){
+    cout<<a<<" ";
+    next= a+b;
+    a= b;
+    b=next;
+  }
+  
 }
