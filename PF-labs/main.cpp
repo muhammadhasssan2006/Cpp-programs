a3f175dd86509c2a88934e72e0f096e23f1fc319
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-04-05 13:26:44 +0500

    Programming Lab Programs

---
+++ PF-labs/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string Name;
+    int age,CGPA;
+    cout << "Enter your name: ";
+    cin>>Name;
+    cout << "Enter your Age: ";
+    cin>> age;
+    cout << "Enter your CGPA: ";
+    cin>> CGPA;
+    cout << "your Name: "<< Name <<endl;
+    cout << " your Age: "<< age <<endl;
+    cout << " your CGPA: "<< CGPA;
+    
+    return 0;
+}
