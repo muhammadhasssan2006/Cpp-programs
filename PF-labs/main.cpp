9a4f7793504dcd7dc5ce41e14995b5cc8891f0f6
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-04-05 14:14:09 +0500

    more done

--- PF-labs/main.cpp
+++ PF-labs/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string Name;
-    int age,CGPA;
-    cout << "Enter your name: ";
-    cin>>Name;
-    cout << "Enter your Age: ";
-    cin>> age;
-    cout << "Enter your CGPA: ";
-    cin>> CGPA;
-    cout << "your Name: "<< Name <<endl;
-    cout << " your Age: "<< age <<endl;
-    cout << " your CGPA: "<< CGPA;
-    
+int main()
+{
+    // 1)Write a program that takes name, age and CGPA from the user and displays them in the
+    // string Name;
+    // int age,CGPA;
+    // cout << "Enter your name: ";
+    // cin>>Name;
+    // cout << "Enter your Age: ";
+    // cin>> age;
+    // cout << "Enter your CGPA: ";
+    // cin>> CGPA;
+    // cout << "your Name: "<< Name <<endl;
+    // cout << " your Age: "<< age <<endl;
+    // cout << " your CGPA: "<< CGPA;
+
+    // 2)Write a program that inputs two numbers and displays:
+    int num1, num2;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "Sum =" << num1 + num2 << endl;
+    cout << "Difference =" << num1 - num2 << endl;
+    cout << "Product =" << num1 * num2 << endl;
+    cout << "Division =" << num1 / num2 << endl;
+
     return 0;
 }
